dd9494d292e18e8d34422e2b1d9d1ddce570de9d
YOUNES-CLOUDIIII <cliiclisaghiri@gmail.com>
2026-03-27 21:20:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-
+app.use(cors({ origin: '*' }));
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
